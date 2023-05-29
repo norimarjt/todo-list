0be345302512be4631f844b78de2feb52f837dbf
norimarjt <106575399+norimarjt@users.noreply.github.com>
2023-05-28 22:17:56 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import Date from './components/Date.js';
+import { handleAddTask, LoadTask } from './components/Task.js';
+
+const button = document.querySelector('[data-button]');
+const input = document.querySelector('[data-input]');
+
+button.addEventListener('click', handleAddTask);
+input.addEventListener('keydown', handleAddTask);
+//button.onclick = handleAddTask;
+
+const span = document.querySelector('.welcome span');
+span.textContent = Date();
+
+LoadTask();
